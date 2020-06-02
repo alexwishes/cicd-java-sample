81648fa3aa97ed32ba75e5663b04bb4fe7e9c403
yangyt001 <yangyt001@foresealife.com>
2020-06-02 09:31:53 +0800

    Deal with "DevToolsActivePort file doesn't exist"

--- src/test/java/com/alexwishes/BaseTest.java
+++ src/test/java/com/alexwishes/BaseTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.*;
 
 import static com.codeborne.selenide.WebDriverRunner.setWebDriver;
@@ -20,8 +21,10 @@ public class BaseTest {
 
 	@BeforeClass
     public void setUp(){
-        System.setProperty("webdriver.chrome.driver","/opt/google/chrome/chromedriver");
-        driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver","/home/yangyt001/Downloads/chromedriver");
+        final ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--no-sandbox");
+        driver = new ChromeDriver(chromeOptions);
     }
 
 
